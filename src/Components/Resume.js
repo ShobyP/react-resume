87d2a339171297a01fc89a2894041d31a91d59eb
unknown <main.shoby@gmail.com>
2025-01-24 10:17:26 -0600

    fixes various compile issues

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,17 +5,36 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
+
+      //Education:
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <p>{education.description}</p></div>
       })
+
+      //Certifications:
+      var certifications = this.props.data.certifications.map(function(certification){
+        return <div key={certification.certification}><h3>{certification.issuedBy}</h3>
+        <div>
+         <p className="info">{certification.certification} <div><em className="date">{certification.validity}</em></div>
+        <div>
+         <a href={certification.credly} target="_blank" className="link"><i className="fa fa-fw fa-external-link-square"  aria-hidden="true"></i>&nbsp; Credly Verification</a>
+         </div>
+        </p>
+        </div>
+        </div>
+      })
+
+      //Work History
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
         </div>
       })
+
+      //Skillset:
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
         return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
@@ -34,6 +53,20 @@ class Resume extends Component {
             <div className="row item">
                <div className="twelve columns">
                  {education}
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Certifications</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {certifications}
                </div>
             </div>
          </div>
